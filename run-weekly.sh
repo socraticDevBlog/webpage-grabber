199f5541449601414bd95204f480de12475c3646
socraticDevBlog <maximebonin@outlook.com>
2020-07-07 21:22:21 -0400

    adding second args as a special label for out name

--- run-weekly.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-source ./env/bin/activate
-
-python3 webpage-grabber.py https://ciao.ca/qui-nous-sommes
-
-
--- webpage-grabber.py
+++ webpage-grabber.py
@@ -6,11 +6,18 @@ from datetime import datetime
 import os
 from tld import get_tld
     
+URL_INDEX = 1
+EXTRA_LABEL_INDEX = 2
+MINIMUM_ARGS_COUNT = 2
+LOG_FILE_NAME = 'history.log'
+OUTPUT_DIRECTORY_NAME = 'output'
+SPACER_CHAR = '_'
+
 
 path = os.getcwd()
 Format_String = "%Y-%m-%d-%H-%M-%S"
 timestamp = datetime.utcnow().strftime(Format_String)
-logname = os.path.join(path, 'history.log')
+logname = os.path.join(path, LOG_FILE_NAME)
 
 logging.basicConfig(filename=logname,
                             filemode='a',
@@ -18,11 +25,13 @@ logging.basicConfig(filename=logname,
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
 
-if len(sys.argv) < 2:
+args_count = len(sys.argv)
+
+if args_count < MINIMUM_ARGS_COUNT:
     logging.warning('Failed: kindly provide a valid URL as 1st parameter: $python webpage-grabber.py https://mysite.cohttp://ftp.nasa.com/m')
     sys.exit('EXIT: Failed: kindly provide a valid URL as 1st parameter: $python webpage-grabber.py https://mysite.com')
 
-url = str(sys.argv[1])
+url = str(sys.argv[URL_INDEX])
 
 logging.info('Downloading html code from: ' + url)
 
@@ -33,14 +42,13 @@ regex = re.compile(
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
 is_valid_url = re.match(regex, url)
 
 if not is_valid_url:
     logging.warning('Failed: invalid url provided (full format required ex.: https//mysite.com): ' + url)
     sys.exit('EXIT: Failed: invalid url provided (full format required ex.: https//mysite.com): ' + url)
 
-downloads_dir = os.path.join(path,'downloads')
+downloads_dir = os.path.join(path,OUTPUT_DIRECTORY_NAME)
 
 res = get_tld(url, as_object=True)
 site_name = res.fld
@@ -59,7 +67,11 @@ if site_content.status_code == 500:
     logging.warning('Failed: 500 - Forbidden or server failure ' + url)
     sys.exit('EXIT: 500 - Forbidden or server failure: ' + url)
 
-file_name = os.path.join(downloads_dir, site_name + '_' + timestamp + '.html')
+if args_count == MINIMUM_ARGS_COUNT:
+    file_name = os.path.join(downloads_dir, site_name + SPACER_CHAR + timestamp + '.html')
+else:
+    extra_label = str(sys.argv[EXTRA_LABEL_INDEX])
+    file_name = os.path.join(downloads_dir, site_name + SPACER_CHAR + extra_label + timestamp + SPACER_CHAR + '.html')
 
 try:
     with open(file_name, 'w', encoding='utf-8') as file:
